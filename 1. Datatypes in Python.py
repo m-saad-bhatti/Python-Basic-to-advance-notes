4f028cd68fc07aa54d9f9ccc113e2ad6c43337d3
Saad <msaadmunirbhatti@gmail.com>
2026-01-06 23:25:00 +0500

    Add files via upload

---
+++ 1. Datatypes in Python.py
@@ -0,0 +1,10 @@
+# 1. Data Types: int, float, str, bool
+number = 10  # int
+pi = 3.14159  # float
+is_active = True  # bool
+message = "Python is awesome!"  # str
+
+print(type(number))  # Prints: <class 'int'>
+print(type(pi))  # Prints: <class 'float'>
+print(type(is_active))  # Prints: <class 'bool'>
+print(type(message))  # Prints: <class 'str'>
