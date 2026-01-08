0e68e761fc44f42610c5eee9a2f73c417d3f97e4
Saad <msaadmunirbhatti@gmail.com>
2026-01-08 23:30:05 +0500

    Add files via upload

---
+++ 2. Input & Output.py
@@ -0,0 +1,5 @@
+# 2. Input and Output
+name = input("Enter your name: ")  # Get user input (string)
+age = int(input("Enter your age: "))  # Get user input (int)
+
+print(f"Hello, {name}! You are {age} years old.")  # Output using f-string
