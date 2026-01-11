016c630ab56b10f3098305879387d3e99672c07d
Saad <msaadmunirbhatti@gmail.com>
2026-01-11 22:25:52 +0500

    Add files via upload

---
+++ 3. Conditional Statements(if-else).py
@@ -0,0 +1,6 @@
+# 3. Conditional Statements (if, else, elif)
+age = int(input("Enter your age: "))  # Get user input (int)
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
